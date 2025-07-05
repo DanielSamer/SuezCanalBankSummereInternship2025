ab995c17156448512ef1457341cd6de25adc66c6
DanielSamer <danielsamer04@gmail.com>
2025-07-05 18:59:50 +0300

    JavaThreads2

--- src/JavaThreads/Main1.java
+++ src/JavaThreads/Main1.java
@@ -29,17 +29,42 @@ public class Main1 {
         System.out.println ("We will start thread 2 here");
         //Making a second thread 
         MyThread thread2 = new MyThread(); 
+
+
         thread2.start(); // Start the thread (After the thread is started, the run method. that i have made in the MyThread class, will be called automatically)
+        // it appears here enaha user thread bas if we wrote that line after we change it to daemon thread el output hait8aiar 3ady 
+
         System.out.println(thread2.isAlive());// Check if the thread is alive 
         System.out.println(thread2.getName());
         thread2.setName("Thread 2");
         System.out.println(thread2.getName());
 
+        //it inherits the priority of the main thread 
+        System.out.println(thread2.getPriority());
 
+        //change the priority of the thread
+        thread2.setPriority(5); // Set the priority of thread2 to 5
+        System.out.println(thread2.getPriority());
 
+        System.out.println(Thread.activeCount()); // Print the number of active threads
 
+        /*There are
+         *********USER THREADS*********
+         * User threads are the main threads that run your application code. They are created by the programmer and can be either foreground or background threads.
+         * * They are typically used for tasks that require user interaction or processing.
+         *********DAEMON THREADS*********
+         * Daemon threads are used for background tasks and do not prevent the JVM from exiting.
+         * * They are typically used for tasks that run in the background, such as garbage collection or monitoring.
+         */
 
+        //Check if the thread is a daemon thread 
+        System.out.println("Is thread2 a daemon thread? " + thread2.isDaemon());
 
+        //Setting the thread as a daemon thread
+        thread2.setDaemon(true);
+        System.out.println("Is thread2 a daemon thread? " + thread2.isDaemon());
+        
+        //thread2.start(); // Start the thread (After the thread is started, the run method. that i have made in the MyThread class, will be called automatically)
     }
 }
 
--- src/JavaThreads/MyThread.java
+++ src/JavaThreads/MyThread.java
@@ -4,6 +4,12 @@ public class MyThread extends Thread{
 
     @Override
     public void run(){
+
+        if (this.isDaemon()) {
+            System.out.println("This is a daemon thread");
+        } else {
+            System.out.println("This is a user thread");
+        }
         System.out.println("This thread is running");
     }
 
